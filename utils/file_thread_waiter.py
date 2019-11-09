70dfb56a7b2a7195456915b64669efb530654442
Brett Henderson <brettrhenderson25@gmail.com>
2019-11-09 15:46:03 -0800

    refined exemple of threading

--- utils/file_thread_waiter.py
+++ utils/file_thread_waiter.py
@@ -5,43 +5,57 @@ import os
 
 needed_file = "/home/brett/Projects/SLICE/test_directory/test.out"
 
-def process_file(path):
-    print("Found file {}".format(os.path.split(path)[1]))
+def process_file(path, start):
+    print("\t\t-Found file {} in {:.2f} seconds".format(os.path.split(path)[1], time.time() - start))
 
 
 def do_something_1():
-    time.sleep(5)
-    print("did something 1")
+    start = time.time()
+    time.sleep(10)
+    print("\t\t-did thing 1 in {:.2f} seconds".format(time.time() - start))
 
 
 def do_something_2():
-    time.sleep(5)
-    print("did something else")
+    start = time.time()
+    time.sleep(4)
+    print("\t\t-did thing 2 in {:.2f} seconds".format(time.time() - start))
 
 
 def do_something_3():
+    start = time.time()
     time.sleep(3)
-    print("did a third thing")
+    print("\t\t- did thing 3 in {:.2f} seconds".format(time.time() - start))
 
 
 def check_for_file(path):
+    start = time.time()
     while True:
         if not os.path.isfile(path):
             time.sleep(1)
         else:
-            process_file(path)
+            process_file(path, start)
             break
 
 
 def main():
-
+    print("Entering Main Loop:")
     thread = threading.Thread(target=check_for_file, args=[needed_file])
     thread.start()
-
-    do_something_1()
-    do_something_2()
-    do_something_3()
-
+    print("\t* Start looking for file test.out")
+    start = time.time()
+    t2 = threading.Thread(target=do_something_1)
+    t2.start()
+    print("\t* Start doing thing 1")
+    t3 = threading.Thread(target=do_something_2)
+    t3.start()
+    print("\t* Start doing thing 2")
+    t4 = threading.Thread(target=do_something_3)
+    t4.start()
+    print("\t* Start doing thing 3")
+    t2.join()
+    t3.join()
+    t4.join()
+    print("Time for 3 tasks combined: {:.2f} seconds".format(time.time() - start))
     thread.join()
 
 
